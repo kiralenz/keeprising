c32e405bd98de7ba341a6b3a201920cf919c7704
kira <kira.lenz@contiamo.com>
2022-12-23 21:16:52 +0100

    dummy data creation

--- keeprising.py
+++ keeprising.py
@@ -1,4 +1,8 @@
 import streamlit as st
 import pandas as pd
 
+feedings = pd.read_parquet('/Users/admin/Documents/GitHub/keeprising/data/feedings.parquet')
+
 st.title('Keep Rising')
+
+st.write(feedings)
