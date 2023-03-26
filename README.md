1cf60cd68677f74b69bee4c54aa56d9224ea2486
kiralenz <kira.lenz@contiamo.com>
2023-03-26 19:43:31 +0200

    split app into multiple pages

--- README.md
+++ README.md
@@ -3,7 +3,8 @@
 Creating a local streamlit web app for sourdough monitoring. 
 
 ## Setup
-I use the anaconda navigator to create an environment using the `environment-keeprising.yml`
+I use the anaconda navigator to create an environment using the `environment-keeprising.yml`.
+Before you can start, you have to create an OpenAI API key. Next you have to store the key `openai_api_key` and your organization ID `openai_organization` in a folder called `config` in a `config.json` file. 
 
 ## Usage
 At the moment:
@@ -12,9 +13,16 @@ At the moment:
 3. go to the folder where everything is stored (local)
 4. `streamlit run keeprising.py`
 Voila. 
+5. If you need to reset the data because you messed things up, I fear you will have to reset to dummy data by running the notebook `dummy_data_creation.ipynb`.
 
 ## Todo
+* transform dummy_data_creation in a python script
 * expand app with plots
     * find out why plot is not shown
 * add feature to add latest data in streamlit (if possible) and save the latest feeding data to local files
 * store path in a external notebook/file to be called
+* functionalize the script
+* split script into a feeding activity, a baking activity and a dashboard activity + help page
+* improve the bread loaf background and store it in a gist
+* store the path in the config.json
+* create a utils.py and store all the tiny functions in there
--- pages/keeprising_dashboard.py
+++ pages/keeprising_dashboard.py
@@ -6,27 +6,68 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
 import matplotlib.ticker as ticker
+import json
+import openai
+import base64
+from ../utils.py import add_bg
+
+# getting keys
+with open('config/config.json') as f:
+    keys = json.load(f)
+openai_api_key = keys['openai_api_key']
+openai_organization = keys['openai_organization']
+openai.organization = openai_organization
+openai.api_key = openai_api_key
+openai.Model.list()
 
 # Variables
 PATH = ('/Users/kiralenz/Documents/keeprising/data/')
+
+# Functions
+# better read functions from utils, but not yet working
+def add_bg(image_file):
+    with open(image_file, "rb") as image_file:
+        encoded_string = base64.b64encode(image_file.read())
+    st.markdown(
+    f"""
+    <style>
+    .stApp {{
+        background-image: url(data:image/{"png"};base64,{encoded_string.decode()});
+        background-size: cover
+    }}
+    </style>
+    """,
+    unsafe_allow_html=True
+    )
+
 
 # Loading data
 feedings = pd.read_parquet(PATH + 'feedings.parquet')
 baked_bread = pd.read_parquet(PATH + 'baked_bread.parquet')
 recyled_dough = pd.read_parquet(PATH + 'recycled_dough.parquet')
 
-st.title('Keep Rising')
+# streamlit page
+st.set_page_config(page_title="Keeprising")
+add_bg_from_local('bread_loaf.png')  
+st.title('Keep Rising - Dashboards')
 
 st.header('Another feeding')
 
 # TODO: Add field for input
 # Adding new feeding data
-date_today = '2022-12-20'
-temperature_today = 19
-feeding_time_today = 10
-initial_height_today = 2.9
-end_height_today = 7.9
-bubble_size_today = 0.3
+st.write('How was your last feeding?')
+date_today = st.date_input('Feeding date')
+# date_today = '2022-12-20'
+temperature_today = st.number_input('Temperature')
+# temperature_today = 19
+feeding_time_today = st.number_input('Feeding duration')
+# feeding_time_today = 10
+initial_height_today = st.number_input('Intial height')
+# initial_height_today = 2.9
+end_height_today = st.number_input('End height')
+# end_height_today = 7.9
+bubble_size_today = st.number_input('Bubble size')
+# bubble_size_today = 0.3
 
 st.write(date_today)
 st.write(temperature_today)
@@ -54,6 +95,7 @@ bacteria_composition = pd.DataFrame({
     'temperature':[20, 25,30, 35],
     'dominant_microbes':['none', 'lactic acid bacteria', 'Acetic acid bacteria', 'sourdough yeast']
 })
+
 
 feedings["bacteria_composition"] = np.where(
     feedings["temperature"] <= 20,
@@ -202,3 +244,22 @@ st.pyplot(bubblesize_dependence_from_temperature2)
 
 # bacteria_composition_plot = sns.catplot(data=feedings, x='bacteria_composition', y='growth_rate', kind='boxen');
 # st.pyplot(bacteria_composition_plot)
+
+
+
+# Help area
+st.header("Help")
+question = st.text_input('Ask your question here')
+# response = openai.ChatCompletion.create(
+#   model="gpt-3.5-turbo",
+#   messages=[
+#         {"role": "system", "content": "You are a bakery educator."},
+#         {"role": "user", "content": question},
+#     ]
+# )
+# answer = response['choices'][0]['message']['content']
+# helper code which is not necessary anymore
+response = 'Answer to question: ' + question
+answer=response
+
+st.write(answer)
